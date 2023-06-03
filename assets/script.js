3fd8cde8eed48c6312d7b4864fa0a3cc8bf1b87d
Merel Jacobs <merel.burleigh@gmail.com>
2023-06-03 09:29:32 -0700

    added buttons and functionality to sections

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,25 @@
 var hoaBtn = document.querySelector("#hoa-card");
 var hoaSection = document.querySelector("#hoa-section");
+var poolRulesBtn = document.querySelector("#poolrules-card");
+var poolRuesSection = document.querySelector("#poolrules-section");
+var otherBtn = document.querySelector("#other");
+var otherSection = document.querySelector("#other-section");
+
 
 hoaBtn.addEventListener("click", function() {
-    hoaSection.style.display("flex");
+    hoaSection.style.display = "flex";
+    poolRuesSection.style.display = "none";
+    otherSection.style.display = "none";
+})
+
+poolRulesBtn.addEventListener("click", function() {
+    poolRuesSection.style.display = "flex";
+    hoaSection.style.display = "none";
+    otherSection.style.display = "none";
+})
+
+otherBtn.addEventListener("click", function() {
+    otherSection.style.display = "flex";
+    poolRuesSection.style.display = "none";
+    hoaSection.style.display = "none";
 })
